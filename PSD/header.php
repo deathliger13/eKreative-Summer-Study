c3ddf42cbe3d8e7255fda8b3474c05fad84002eb
deathliger666 <oleg.golovin13@gmail.com>
2016-07-08 14:38:20 +0300

    php update

---
+++ PSD/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<header>
+    <div class="containerPanakeia">
+        <div>
+            <p class="logoPanakeia">Panakeia</p>
+            <p class=" logoFree">Free PSD template</p>
+        </div>
+        <div>
+            <ul class="list">
+                <li><a href="#">Fusce at</a></li>
+                <li><a href="#">Facilisis</a></li>
+                <li><a href="#">Ante donec</a></li>
+                <li><a href="#">Blandit rhoncus</a></li>
+                <li><a href="#">Placerat</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
