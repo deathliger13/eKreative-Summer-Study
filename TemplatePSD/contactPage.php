f6932437658cdd4c9da6994e0e9290bbadfbc7fa
deathliger666 <oleg.golovin13@gmail.com>
2016-07-13 11:32:45 +0300

    PhP makeup v0.5

--- TemplatePSD/contactPage.php
+++ TemplatePSD/contactPage.php
@@ -1,4 +1,16 @@
 <?php include 'header.php'; ?>
+<div class="contactBody">
+    <div class="contactContent">
+        <h2 class="contactHeader">GET IN TOUCH</h2>
+        <p class="contactTxt">1600 Pennsylvania Ave NW, Washington, DC 20500, United States of America. Tel: (202) 456-1111.</p>
+        <form action="#" class="contactForm">
+            
+            
+        </form>
 
+    </div>
+
+
+</div>
 
 <?php include 'footer.php'; ?>
