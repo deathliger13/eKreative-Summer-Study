fa430b6c2c06d4599a45f5a57aa802ebf664326e
deathliger666 <oleg.golovin13@gmail.com>
2016-07-20 17:15:32 +0300

    New PSD template Notify - responsible so close

---
+++ NotifyPSD/index.php
@@ -0,0 +1,54 @@
+<?php include 'header.php'; ?>
+<div class="index-content">
+    <div class="background">
+        <div class="txt">
+            <p class="noty-title">Notify</p>
+            <p class="noty-txt">A great new free psd theme to showcase your new application. </p>
+            <div class="noty">
+                <figure class="round-a">
+                    <img src="img/apple.png" alt="404" class="apple">
+                </figure>
+                <figure class="round-an">
+                    <img src="img/android.png" alt="404" class="android">
+                </figure>
+                <figure class="round-w">
+                    <img src="img/windows.png" alt="404" class="windows">
+                </figure>
+            </div>
+        </div>
+    </div>
+    <div class="content-1">
+        <div class="c-1">
+            <img src="img/wheelee.png" alt="404">
+            <h3>Editable Theme</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
+        </div>
+        <div class="c-1">
+            <img src="img/star.png" alt="">
+            <h3>Flat Design</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
+        </div>
+        <div class="c-1">
+            <img src="img/earth.png" alt="">
+            <h3>Reach Your Audience</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.</p>
+        </div>
+    </div>
+    <div class="content-2">
+        <div class="c-2">
+            <h3>Get Notified Of Any Updates!</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla nisl congue
+                congue. Maecenas nec condimentum libero, at elementum mauris. Phasellus eget nisi dapibus, ultricies
+                nisl at, hendrerit risusuis ornare luctus id sollicitudin ante lobortis at.
+            </p>
+            <input type="text" name="Email" id="1">
+            <button>Notify</button>
+        </div>
+        <div class="player">
+            <iframe width="545" height="361" src="https://www.youtube.com/embed/AHsdQPjMMNk" frameborder="0"
+                    allowfullscreen></iframe>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
